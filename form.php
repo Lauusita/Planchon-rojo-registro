ef5ced88e50e3d70f22f4577d3179c939487ef1e
Lauusita <laurasofiaartetayepes@gmail.com>
2023-06-26 20:33:23 -0500

    FIRST UPDATE: adding html, css and php part

---
+++ form.php
@@ -0,0 +1,24 @@
+<?php
+    $mes = $_POST['mes'];
+    $dia = $_POST['dia'];
+    $semana = $_POST['semana'];
+    $plata = $_POST['plata'];
+
+    echo $mes;
+    
+
+$connect =mysqli_connect('localhost','root' , '', 'planchon_rojo');
+
+$insertar = "INSERT INTO registrodinero (mes, dia, semana, plata) values('$mes', '$dia', '$semana', '$plata')";
+
+$enviar = mysqli_query($connect, $insertar);
+
+if ($enviar){
+    echo "<h1> Los datos se han enviado </h1>";
+}
+else{
+    echo "<h1> Los datos no se han enviado </h1>";
+}
+
+mysqli_close($connect);
+?>
